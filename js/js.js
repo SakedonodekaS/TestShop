85b4bd018d9df077b2235730e4895248eb2cd35c
Hyunwoo <107166410+SakedonodekaS@users.noreply.github.com>
2022-06-09 16:49:43 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+src="https://code.jquery.com/jquery-3.6.0.js"
+    $(document).ready(function(){
+    $('.slider').bxSlider({
+        auto:true,
+        speed:300,
+        pager: null,
+        controls: null,
+    });
+    });
